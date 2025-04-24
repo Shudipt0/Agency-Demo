59c3142d751567e28fe939a587fb9637b49bff6c
Shudipto <royshudipto318@gmail.com>
2025-04-24 21:32:27 +0600

    adding prisma

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import { Features } from "@/components/Features";
 import Footer from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
 import { Pricing } from "@/components/Pricing";
+import { prisma } from "@/lib/utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const services = await prisma.service.findMany();
+  console.log(services)
   return (
     <div className="bg-[#080809]" >    
      <HeroSection/>
